47f6a66a7e5d7d79ffd861e599e719f2414dacbb
suniltechfun <sunilforu@gmail.com>
2019-02-05 22:58:38 +0800

    Complete working code for the book

---
+++ sensor-project/server/obtain-readings.js
@@ -0,0 +1,9 @@
+const sensor = require('node-dht-sensor')
+
+sensor.read(11, 23, function(err, temperature, humidity)
+	{
+		if (!err) {
+			console.log('temp: ' + temperature.toFixed(1) + ' °C' + 'humidity: ' + humidity.toFixed(1) + '%')
+		}
+	}
+);
